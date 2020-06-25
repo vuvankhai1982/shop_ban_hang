ced08e8de46ff77c763855b680f419fc17383eb2
Khai.Hoan <khaihoan250382@gmail.com>
2020-06-25 22:31:57 +0700

    theme frontend

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
    public function index(){
-       return view('admin.index');
+       return view('frontend.product');
    }
 }
