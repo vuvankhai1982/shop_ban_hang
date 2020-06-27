39f73da2742743be897da20fbe0ab18a35cfbc3e
Khai.Hoan <khaihoan250382@gmail.com>
2020-06-27 23:06:53 +0700

    login

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,20 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('conv', function (){
+    $users=App\users::all();
+    foreach ($users as $user){
+        $a = App\users::find($user->id);
+        $a->password=bcrypt('12345678');
+        $a->save();
+    }
+});
+Route::prefix('admin')
+
+//    ->namespace('Admin')
+    ->group(function (){
+
+        Route::get('/login','LoginController@getLogin')->name('get_login');
+        Route::post('/login', 'LoginController@postLogin')->name('post_login');
+
+});
