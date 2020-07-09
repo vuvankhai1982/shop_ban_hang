71963649af243c2f01741eb627896f6cfae94d16
Khai.Hoan <khaihoan250382@gmail.com>
2020-07-09 16:40:41 +0700

    product

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'products';
+    protected $fillable = [
+        'name',
+        'category_id',
+        'description',
+        'content',
+        'status_id',
+        'unit_price',
+        'promotion_price',
+        'image',
+        'thumbnail_image'
+    ];
+    public function product_categories(){
+      return  $this->belongsTo('App\Models\Product', 'category_id', 'id');
+
+    }
+}
